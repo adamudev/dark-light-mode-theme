946f676fd9b56415e2795d7c84e73607b8613a4d
Adamu Abubakar <adamudev@gmail.com>
2020-09-20 23:30:40 +0100

    Add Darkmode and Lightmode icons

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const toggleButton = document.querySelector('#toggle-button');
 toggleButton.addEventListener('click', () => {
 	document.body.classList.toggle('dark');
 });
+
+function changeImage() {
+	let img = document.getElementById('toggle-button').src;
+	if(img.indexOf('moon.svg') != -1) {
+		document.getElementById('toggle-button').src = 'images/sun.svg';
+	} else {
+		document.getElementById('toggle-button').src = 'images/moon.svg';
+	}
+}
